b5042ae5f4f916b1cf50c8a903613af04a90886b
ShahinGH <ghaneishahin73@gmail.com>
2020-04-01 18:27:45 +0200

    Updates

---
+++ precache-manifest.a4735f86ffb61e11a38b920380392dfd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9664a9254157b697e12bf20b19130928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d5146c21284a968a0d3",
+    "url": "/static/css/2.f44cb463.chunk.css"
+  },
+  {
+    "revision": "b596b44c1b84b30c9dbe",
+    "url": "/static/css/main.0575ed2b.chunk.css"
+  },
+  {
+    "revision": "7d5146c21284a968a0d3",
+    "url": "/static/js/2.1365d3bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1365d3bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b596b44c1b84b30c9dbe",
+    "url": "/static/js/main.38e51083.chunk.js"
+  },
+  {
+    "revision": "c0a68b4e3ffa8942f639",
+    "url": "/static/js/runtime-main.54ef210b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
